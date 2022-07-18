ef9c101c7b269fce777697946a1337ccac4b376e
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-07-18 14:13:56 +0800

    Added function to scrape all article links in .html file, returns an array containing the full URLs.

--- newsAggregator.py
+++ newsAggregator.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 
 import requests
-import re
-from bs4 import BeautifulSoup
+import bs4
 
 url = 'https://portswigger.net/daily-swig'
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
+htmlFile = requests.get(url)
 
-#print(soup.find_all('a', class_ = "noscript-post").get('href'))
-
-for link in soup.find_all('a', class_ = "noscript-post", attrs={'href': re.compile("^/")}):
-    # display the actual urls
-    print(link.get('href')) 
-
-    
+def getNewsLinks(html):
+    # Create Soup object and find all instances of class, returns array with URLs
+    soup = bs4.BeautifulSoup(html.content, 'html.parser')
+    lists = []
+    while True:
+        for link in soup.find_all('a', class_ = "noscript-post", href=True):
+            lists.append("https://portswigger.net" + link['href'])
+        return lists
