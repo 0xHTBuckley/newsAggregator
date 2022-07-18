52f99fe4e470ff8cbb027fdf2a325a93e2dc5dcd
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-07-18 20:33:44 +0800

    Added the email sending function, alongside splitting the email headers and email body into a seperate file that is imported into the main.

--- newsAggregator.py
+++ newsAggregator.py
@@ -2,31 +2,48 @@
 
 import requests
 import bs4
-from emailComponents import formatEmail
+import smtplib
+import ssl
+from emailComponents import formatEmail, emailMsg, password
 
-url = 'https://portswigger.net/daily-swig'
-htmlFile = requests.get(url)
+scrapeURL = 'https://portswigger.net/daily-swig'
+htmlFile = requests.get(scrapeURL)
 
 def getArticleLinks(html):
-    # Create Soup object and find all instances of class, returns array with URLs
-    soup = bs4.BeautifulSoup(html.content, 'html.parser')
-    lists = []
+    # Create Soup object and find all instances of "noscript-post" class, returns array with URLs.
+    
+    soupObj = bs4.BeautifulSoup(html.content, 'html.parser')
+    URLs = []
+    
     while True:
-        for link in soup.find_all('a', class_ = "noscript-post", href=True):
-            lists.append("https://portswigger.net" + link['href'])
-        return lists
+        for link in soupObj.find_all('a', class_ = "noscript-post", href=True):
+            URLs.append("https://portswigger.net" + link['href'])
+        return URLs
 
 def getArticleDescriptions(html):
-    # Create Soup object and find descriptions of the articles
-    soup = bs4.BeautifulSoup(html.content, 'html.parser')
-    lists = []
+    # Create Soup object and find descriptions of the articles.
+    
+    soupObj = bs4.BeautifulSoup(html.content, 'html.parser')
+    descriptions = []
+    
     while True:
-        for link in soup.find_all('span', class_ = "main"):
-            lists.append(link.text)
-        return lists
-goddamn = getNewsLinks(htmlFile)
-desc = getNewssLinks(htmlFile)
+        for link in soupObj.find_all('span', class_ = "main"):
+            descriptions.append(link.text)
+        return descriptions
 
-email = formatEmail(desc, goddamn)
-print(email)
+def sendEmail(password, email):
 
+    contextDefault = ssl.create_default_context()
+
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", port = 465, context = contextDefault) as server:
+            server.login(email['From'], password)
+            server.sendmail(email['From'], email['To'], email.as_string())
+    except Exception:
+        print("Error with sending email!")
+
+articleLink = getArticleLinks(htmlFile)
+
+emailMsg.set_content(formatEmail(articleLink))
+
+sendEmail(password, emailMsg)
