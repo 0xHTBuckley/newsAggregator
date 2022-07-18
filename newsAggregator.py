7ec374d562d57b4193c40497c32f62afa23a0255
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-07-18 14:45:30 +0800

    New function to scrape descriptions of the articles, returns in array form.

--- newsAggregator.py
+++ newsAggregator.py
@@ -2,11 +2,12 @@
 
 import requests
 import bs4
+from emailComponents import formatEmail
 
 url = 'https://portswigger.net/daily-swig'
 htmlFile = requests.get(url)
 
-def getNewsLinks(html):
+def getArticleLinks(html):
     # Create Soup object and find all instances of class, returns array with URLs
     soup = bs4.BeautifulSoup(html.content, 'html.parser')
     lists = []
@@ -14,3 +15,18 @@ def getNewsLinks(html):
         for link in soup.find_all('a', class_ = "noscript-post", href=True):
             lists.append("https://portswigger.net" + link['href'])
         return lists
+
+def getArticleDescriptions(html):
+    # Create Soup object and find descriptions of the articles
+    soup = bs4.BeautifulSoup(html.content, 'html.parser')
+    lists = []
+    while True:
+        for link in soup.find_all('span', class_ = "main"):
+            lists.append(link.text)
+        return lists
+goddamn = getNewsLinks(htmlFile)
+desc = getNewssLinks(htmlFile)
+
+email = formatEmail(desc, goddamn)
+print(email)
+
